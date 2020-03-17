0afd7163a88083df424c382d5dd4983cc5d6036b
zerstoyber <jacobusstoy@aol.com>
2020-03-17 23:15:23 +0100

    update

--- src/party.js
+++ src/party.js
@@ -11,13 +11,17 @@ function setup() {
   var y = elmnt.scrollHeight;
 
   canvas = createCanvas(windowWidth, y);
+//  canvas = createCanvas(windowWidth, windowHeight);
+
   canvas.position(0, 0);
   canvas.style('z-index', '-1')
 }
 
 function draw() {
-  stroke(255);
+//  stroke(255);
+  noStroke();
   if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+//    line(mouseX, mouseY, pmouseX, pmouseY);
+  rect(mouseX, mouseY, 30, 30);
   }
 }
